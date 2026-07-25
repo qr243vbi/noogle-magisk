de43336756c5d466ecad039f8e1a19091f446bbc
qr243vbi <qr243vbi@atomicmail.io>
2026-07-26 05:26:50 +0800

    update

--- src/module-microg/action.sh
+++ src/module-microg/action.sh
@@ -6,10 +6,10 @@ echo "[P] Module self-check..."
 echo
 remove_package_updates google
 
-echo
-echo
-grant_microg_permissions
+#echo
+#echo
+#grant_microg_permissions
 
 echo
-echo
+#echo
 echo "[I] Done fixing module."
